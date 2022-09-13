1be95ccf544314fb7d817dd5884dbc958da6ccfe
Sourcery AI <>
2022-09-13 13:07:40 +0000

    'Refactored by Sourcery'

--- src/Watcher/analysis.py
+++ src/Watcher/analysis.py
@@ -6,11 +6,11 @@ import time_operations as to
 
 def extract_data(date):
     user = os.getlogin()
-    path = "/home/" + user +"/.cache/Watcher/raw_data/"
+    path = f"/home/{user}/.cache/Watcher/raw_data/"
     filename = path + date + ".csv"
 
-    l = list() # l = list of  (app_name, time spent on app on that session)
-    d = dict()
+    l = []
+    d = {}
 
     if os.path.isfile(filename):
         with open(filename, 'r') as file:
@@ -18,7 +18,7 @@ def extract_data(date):
             for row in reader:
                 for column in row:
                     l.append([column[18::],column[9:17]])
-                    d.update({column[18::]: "O"})
+                    d[column[18::]] = "O"
     else:
         None
 
@@ -27,34 +27,31 @@ def extract_data(date):
 
 # creating dictionary to store time spend on each applicaitons on that particular day
 def final_report(window_opened, time_spent):
-    report = dict()
+    report = {}
 
     for i in window_opened: # i is applications name
         time = '00:00:00'
         for j in time_spent: # j is list of applicaions_name and  time_spent in particular session
             if i == j[0]:
                 time = to.time_addition(j[1], time)
-                report.update({i:time})
+                report[i] = time
 
     #print(report)
-    if "User-logged-in" in report.keys():
+    if "User-logged-in" in report:
         report.pop("User-logged-in")
-    if "AFK" in report.keys():
+    if "AFK" in report:
         report.pop("AFK")
-    if "Unknown" in report.keys():
+    if "Unknown" in report:
         report.pop("Unknown")
     # sort report dictonary in decreasing order of Usages
-    sorted_values = []
-    for x,y in report.items():
-        sorted_values.append(to.convert_into_sec(y))
-
+    sorted_values = [to.convert_into_sec(y) for y in report.values()]
     sorted_values.sort(reverse=True)
-    sorted_report = dict()
+    sorted_report = {}
 
     for i in sorted_values:
         for x, y in report.items():
             if to.convert_into_sec(y) == i:
-                sorted_report.update({x:y})
+                sorted_report[x] = y
 
     return sorted_report
 
@@ -77,22 +74,22 @@ def week_dates(theday=datetime.date.today()):
 
 def weekday_from_date(date):
     day = os.popen('''date -d "'''+ date + '''" +%a''').read()
-    return day[0:-1]
+    return day[:-1]
 
-def weekly_logs(week = str(os.popen('''date +"W%V-%Y"''').read()[0:-1])):
+def weekly_logs(week=str(os.popen('''date +"W%V-%Y"''').read()[:-1])):
     user = os.getlogin()
-    filename = "/home/"+user+"/.cache/Watcher/Analysis/"+week+".csv"
+    filename = f"/home/{user}/.cache/Watcher/Analysis/{week}.csv"
     with open(filename, "w") as csvfile:
         csvwriter = csv.writer(csvfile, delimiter='\t')
         #csvwriter.writerow(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"])
 
-        if os.popen('''date +"W%V-%Y"''').read()[0:-1] == week:
+        if os.popen('''date +"W%V-%Y"''').read()[:-1] == week:
             dates = week_dates()
         else:
             dates = week_dates(get_sunday_of_week(week))
 
-        window_opened = list()
-        time_spent = list()
+        window_opened = []
+        time_spent = []
         for i in dates:
             window_opened, time_spent = extract_data(i)
             Total_screen_time = "00:00:00"
--- src/Watcher/watch_log.py
+++ src/Watcher/watch_log.py
@@ -8,17 +8,17 @@ from time_operations import time_difference
 # get current time whenever the function is called
 def get_time():
     t = os.popen('''date +"%T"''').read()
-    return t[0:-1]
+    return t[:-1]
 
 # get date of today
 def get_date():
     d = os.popen('''date +"%Y-%m-%d"''').read()
-    return d[0:-1]
+    return d[:-1]
 
 def append_line_in_csv(date, opened_time, closed_time, window_name):
     user = os.getlogin()
     time_spent = time_difference(opened_time, closed_time)
-    filename = "/home/"+os.getlogin()+"/.cache/Watcher/raw_data/"+date+".csv"
+    filename = f"/home/{os.getlogin()}/.cache/Watcher/raw_data/{date}.csv"
     Data = [closed_time, time_spent,  window_name]
     with open(filename, 'a') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter='\t')
@@ -30,12 +30,12 @@ def append_line_in_csv(date, opened_time, closed_time, window_name):
 # TODO: AFK feature devlopement (it will be developed after completing alpha product (after whole project up end running)
 
 def log_creation():
-    filename = "/home/"+os.getlogin()+"/.cache/Watcher/raw_data/"+get_date()+".csv"
+    filename = f"/home/{os.getlogin()}/.cache/Watcher/raw_data/{get_date()}.csv"
     if not(os.path.isfile(filename)):
         with open(filename, 'a') as csvfile:
             csvwriter = csv.writer(csvfile, delimiter='\t')
             csvwriter.writerow([get_time(), "00:00:00", ""])
-    logout_time = os.popen("tail -n1 " + filename).read().split("\t")[0]
+    logout_time = os.popen(f"tail -n1 {filename}").read().split("\t")[0]
     append_line_in_csv(get_date(), logout_time, get_time(), "User-logged-in")
 
     afk = False
@@ -48,10 +48,17 @@ def log_creation():
         if (y.returned_from_afk(afk, afkTimeout)):
             previous_window = "AFK"
             if os.path.exists(filename):
-                opened_at = os.popen(" tail -n1 "+ filename).read().split("\t")[0]
+                opened_at = os.popen(f" tail -n1 {filename}").read().split("\t")[0]
             else:
                 previous_date = os.popen("""date -d "1 day ago" '+%Y-%m-%d'""").read()[:-1]
-                opened_at = os.popen(" tail -n1 ~/.cache/Watcher/raw_data/"+previous_date+".csv").read().split("\t")[0]
+                opened_at = (
+                    os.popen(
+                        f" tail -n1 ~/.cache/Watcher/raw_data/{previous_date}.csv"
+                    )
+                    .read()
+                    .split("\t")[0]
+                )
+
             afk = False
 
         if (x.is_window_changed(previous_window, afk, afkTimeout) and not afk):
@@ -63,11 +70,11 @@ def log_creation():
                 closed_at = get_time() # for next_window its the opening time
 
             date = get_date()
-            filename = "/home/"+os.getlogin()+"/.cache/Watcher/raw_data/"+date+".csv"
+            filename = f"/home/{os.getlogin()}/.cache/Watcher/raw_data/{date}.csv"
             append_line_in_csv(date, opened_at, closed_at, previous_window)
 
 if __name__ == "__main__":
     #log_creation()
-    filename = "/home/"+os.getlogin()+"/.cache/Watcher/raw_data/"+get_date()+".csv"
-    opened_at = os.popen(" tail -n1 "+ filename).read().split("\t")[0]
+    filename = f"/home/{os.getlogin()}/.cache/Watcher/raw_data/{get_date()}.csv"
+    opened_at = os.popen(f" tail -n1 {filename}").read().split("\t")[0]
     print(opened_at)
